41de7a1ab7b1c1285cd14e8c2e292bfe3c4cee60
Wagner Maciel <wagnermaciel@wagnermaciel-macbookpro.roam.corp.google.com>
2019-11-20 15:22:17 -0800

    implemented naive/temporary deploy for proof of concept

--- src/engine/engine.ts
+++ src/engine/engine.ts
@@ -1,25 +1,19 @@
 import { logging } from '@angular-devkit/core';
 import * as fse from 'fs-extra';
+import { execSync } from 'child_process';
 
 import { Schema } from '../deploy/schema';
 
-// TODO: add your deployment code here!
 export async function run(
   dir: string,
   options: Schema,
   logger: logging.LoggerApi
 ) {
   try {
-    options.targetDir = options.targetDir || '/example-folder';
-
-    if (!(await fse.pathExists(options.targetDir))) {
-      throw new Error(`Target directory ${options.targetDir} does not exist!`);
-    }
-
-    await fse.copy(dir, options.targetDir);
+    execSync('gcloud app deploy', { stdio: 'inherit' });
 
     logger.info(
-      '🚀 Successfully published via @wagnermaciel/gcp-deploy! Have a nice day!'
+      '🚀 Successfully published to Google Cloud Platform! Have a nice day!'
     );
   } catch (error) {
     logger.error('❌ An error occurred!');
